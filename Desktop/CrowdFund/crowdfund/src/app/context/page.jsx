3a5ea5c8cee6f2e5a203eaa59fa0a034fed4d388
Mannan Khan <78471126+Dev-Mannan@users.noreply.github.com>
2024-01-23 22:27:35 +0530

    Delete Desktop directory

--- Desktop/CrowdFund/crowdfund/src/app/context/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export {default as Profile} from './Profile';
-export {default as Home} from './Home';
-export {default as Live} from './Live';
-export {default as campaign} from './campaign';
-
--- Desktop/Token_Stake/client/src/utils/HandleAccountChange.jsx
+++
@@ -1,7 +0,0 @@
-export const HandleAccountChange = async (setState) => {
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-
-    const selectedAccount = accounts[0];
-    console.log(selectedAccount)
-    setState(prevState=>({...prevState,account:selectedAccount}))
-}
