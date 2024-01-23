3066dda787c31f821eaf15d715f7f20c9fc7f63c
Abdul Mannan Khan <mannankhan0770@gmail.com>
2024-01-23 22:24:10 +0530

    Change in responsive design

---
+++ Desktop/CrowdFund/crowdfund/src/app/Home/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='w-full flex justify-center items-center p-5 text-3xl font-bold'>
+        <h1>Home</h1>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Next</button>
+    </div>
+  )
+}
+
+export default Home
